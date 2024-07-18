e44d7d80d8e4e65b1d4a358e756717945813a90f
Kiranpal Juss <kiranpal.juss.external@eviden.com>
2024-07-18 13:56:12 +0100

    DOC-1287 initial commit for dynamic message body

--- cypress/integration/features/06_SendRecieveMessages.feature
+++ cypress/integration/features/06_SendRecieveMessages.feature
@@ -1,9 +1,16 @@
 Feature: Service Bus Messaging
 
-  Scenario: Sending a message to the service bus topic
+  Scenario Outline: Sending a message to the service bus topic "<sendToTopicName>"
     Given I visit the "Request Editor" homepage
     And I make a note of the "Requests awaiting reporting data" count
-    And I create a message with a unique agreement number
-    When I send a message to the service bus topic "ffc-pay-debt-data-test"
-    Then the message should be received successfully for the service bus topic "ffc-pay-debt-data-test"
+    When I create a message for the service bus topic "<sendToTopicName>" and update the following keys:
+      | sbi   |
+      | frn   |
+      | value |
+    When I send the updated message to the service bus topic "<sendToTopicName>"
+    Then the message should be received successfully for the service bus topic "<receiveOnTopicName>"
     And the "Requests awaiting reporting data" count has increased by 1
+
+    Examples:
+      | sendToTopicName        | receiveOnTopicName     |
+      | ffc-pay-debt-data-test | ffc-pay-debt-data-test |
--- cypress/integration/steps/sendReceiveMessagesSteps.js
+++ cypress/integration/steps/sendReceiveMessagesSteps.js
@@ -1,8 +1,10 @@
-/* global Given, Then */
+/* global Given, When, Then */
 
-Given('I send a message to the service bus topic {string}', (topicName) => {
-  cy.fixture('messageBody.json').then((messageBody) => {
-    cy.sendMessage(messageBody, topicName);
+import RandExp from 'randexp';
+
+Given('I send the updated message to the service bus topic {string}', (topicName) => {
+  cy.get('@updatedMessageBody').then((updatedMessageBody) => {
+    cy.sendMessage(updatedMessageBody, topicName);
   });
 });
 
@@ -11,7 +13,7 @@ Then('the message should be received successfully for the service bus topic {str
   cy.fetchReceivedMessages().then((messages) => {
     expect(messages.length).to.be.greaterThan(0);
 
-    cy.fixture('messageBody.json').then((expectedMessage) => {
+    cy.get('@updatedMessageBody').then((expectedMessage) => {
       // Find the message that matches the expected one
       const receivedMessage = messages.find(message => message.correlationId === expectedMessage.correlationId);
 
@@ -20,19 +22,107 @@ Then('the message should be received successfully for the service bus topic {str
   });
 });
 
-Given('I create a message with a unique agreement number', () => {
-  // Function to generate a random agreement number
-  function generateRandomAgreementNumber (prefix) {
-    const randomDigits = Math.floor(100000 + Math.random() * 900000); // Generates a random 6-digit number
-    return `${prefix}${randomDigits}`;
-  }
+When('I create a message for the service bus topic {string} and update the following keys:', (topicName, dataTable) => {
+  const filePath = `cypress/fixtures/messageTemplates/${topicName}.json`;
 
-  // Read the JSON file using cy.fixture
-  cy.fixture('messageBody.json').then((messageBody) => {
+  // Utility function to generate a random value based on custom rules
+  const generateRandomValue = (key, existingValue) => {
+    if (key === 'schedule') {
+      // Ensure schedule is one of Q1, Q2, Q3, or Q4 and not the existing value
+      const options = ['Q1', 'Q2', 'Q3', 'Q4'].filter(option => option !== existingValue);
+      return options[Math.floor(Math.random() * options.length)];
+    } else if (key === 'agreementNumber') {
+      // Ensure the first 3 letters are always SIP and followed by 13 random digits
+      return 'SIP' + Math.floor(1000000000000 + Math.random() * 9000000000000).toString();
+    } else if (key === 'invoiceNumber') {
+      // Preserve all letters, only change the integers, and keep the part V001 intact
+      return existingValue.replace(/\d(?![^V]*$)/g, () => Math.floor(Math.random() * 10));
+    } else if (key === 'frn' || key === 'sbi') {
+      // Preserve the length of FRN and SBI
+      return existingValue.toString().replace(/\d/g, () => Math.floor(Math.random() * 10));
+    } else if (typeof existingValue === 'string') {
+      // General pattern for strings
+      let pattern = '^';
+      for (const char of existingValue) {
+        if (char.match(/\d/)) {
+          pattern += '\\d';
+        } else if (char.match(/[a-zA-Z]/)) {
+          pattern += '[a-zA-Z]';
+        } else {
+          pattern += '\\' + char;
+        }
+      }
+      pattern += '$';
+      return new RandExp(pattern).gen();
+    } else if (typeof existingValue === 'number') {
+      // If the value is a number
+      return Math.floor(Math.random() * 10000);
+    } else if (typeof existingValue === 'boolean') {
+      // If the value is a boolean
+      return Math.random() < 0.5;
+    } else if (existingValue instanceof Array) {
+      // If the value is an array
+      return existingValue.map(item => generateRandomValue(key, item));
+    } else if (existingValue instanceof Object) {
+      // If the value is an object
+      const newObj = { ...existingValue };
+      for (const nestedKey in newObj) {
+        newObj[nestedKey] = generateRandomValue(nestedKey, newObj[nestedKey]);
+      }
+      return newObj;
+    }
+    return null;
+  };
+
+  // Function to update the specified keys in the message body
+  const updateKeys = (obj, updates, valuesMap = {}) => {
+    let keyUpdated = false;
+    for (const key in obj) {
+      if (Object.prototype.hasOwnProperty.call(obj, key)) {
+        if (typeof obj[key] === 'object' && obj[key] !== null) {
+          const updated = updateKeys(obj[key], updates, valuesMap);
+          if (updated) {
+            keyUpdated = true;
+          }
+        } else if (updates.includes(key)) {
+          // Use the same new value for all instances of the same key
+          if (!(key in valuesMap)) {
+            valuesMap[key] = generateRandomValue(key, obj[key]);
+          }
+          obj[key] = valuesMap[key];
+          keyUpdated = true;
+        }
+      }
+    }
+    return keyUpdated;
+  };
+
+  // Function to generate a random agreement number with SIP prefix
+  const generateRandomAgreementNumber = () => {
+    const randomDigits = Math.floor(1000000000000 + Math.random() * 9000000000000); // Generates a random 13-digit number
+    return `SIP${randomDigits}`;
+  };
+
+  // Function to update nested invoice numbers
+  const updateInvoiceNumbers = (obj, currentAgreementNumber, newAgreementNumber) => {
+    for (const key in obj) {
+      if (Object.prototype.hasOwnProperty.call(obj, key)) {
+        if (typeof obj[key] === 'object' && obj[key] !== null) {
+          updateInvoiceNumbers(obj[key], currentAgreementNumber, newAgreementNumber);
+        } else if (typeof obj[key] === 'string' && obj[key].includes(currentAgreementNumber)) {
+          obj[key] = obj[key].split(currentAgreementNumber).join(newAgreementNumber);
+        }
+      }
+    }
+  };
+
+  cy.readFile(filePath).then((messageBody) => {
+    // Convert data table to an array of keys
+    const updates = dataTable.rawTable.flat();
+
     // Extract the current agreement number and generate a new one
     const currentAgreementNumber = messageBody.agreementNumber;
-    const prefix = currentAgreementNumber.charAt(0);
-    const newAgreementNumber = generateRandomAgreementNumber(prefix);
+    const newAgreementNumber = generateRandomAgreementNumber();
 
     // Update the agreement number in the main object and invoice lines
     messageBody.agreementNumber = newAgreementNumber;
@@ -41,13 +131,16 @@ Given('I create a message with a unique agreement number', () => {
     });
 
     // Update the agreement number in the invoice number
-    const invoiceNumberParts = messageBody.invoiceNumber.split(currentAgreementNumber);
-    messageBody.invoiceNumber = invoiceNumberParts.join(newAgreementNumber);
+    updateInvoiceNumbers(messageBody, currentAgreementNumber, newAgreementNumber);
 
-    // Write the updated data back to the JSON file
-    const updatedMessageBody = JSON.stringify(messageBody, null, 2);
+    const keyExists = updateKeys(messageBody, updates);
 
-    // Save the updated messageBody back to the fixture file
-    cy.writeFile('cypress/fixtures/messageBody.json', updatedMessageBody);
+    if (!keyExists) {
+      cy.log('None of the specified keys were found in the message body');
+    }
+
+    cy.wrap(messageBody).as('updatedMessageBody');
+    cy.log(`Message for topic ${topicName} created and updated`);
+    cy.log('Updated keys:', updates); // Log the updated keys
   });
 });
