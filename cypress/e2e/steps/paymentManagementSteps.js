adb8ce5760b8865f8852e09e54c8d7f25268009b
SvendJK <47521171+SvendJK@users.noreply.github.com>
2026-08-04 14:19:15 +0100

    Updated Reset Payment request feature to changes in dev+local

--- cypress/e2e/steps/paymentManagementSteps.js
+++ cypress/e2e/steps/paymentManagementSteps.js
@@ -1349,7 +1349,7 @@ Then (/^on the Reset payment request page I confirm that "(.*)" is displayed$/, 
   case 'page description':
     resetPaymentRequestPage.pageDescription().should('be.visible').containsWithoutWhitespace( 'Invoice number'); break
   case 'page instructions':
-    resetPaymentRequestPage.pageInstructions().should('be.visible').containsWithoutWhitespace( 'Enter the DAX formatted invoice number, for example S1234567S1234567V001'); break
+    resetPaymentRequestPage.pageInstructions().should('be.visible').containsWithoutWhitespace( 'For example S123456789012345V001'); break
   case 'invoice number field':
     resetPaymentRequestPage.invoiceNumberField().should('be.visible').and('have.attr', 'type', 'text'); break
   case 'reset button':
@@ -1359,16 +1359,9 @@ Then (/^on the Reset payment request page I confirm that "(.*)" is displayed$/, 
     resetPaymentRequestPage.errorMessage().should('be.visible').containsWithoutWhitespace( 'Payment request', 'does not exist'); break
   case 'enter a valid invoice number error':
     resetPaymentRequestPage.errorTitle().should('be.visible').containsWithoutWhitespace( 'There is a problem')
-    resetPaymentRequestPage.errorMessage().should('be.visible').containsWithoutWhitespace( 'ValidationError: Enter a valid invoice number'); break
+    resetPaymentRequestPage.errorMessage().should('be.visible').containsWithoutWhitespace( 'Error: Enter an invoice number'); break
   case 'payment request successfully reset message':
-    resetPaymentRequestPage.successTitle().should('be.visible').containsWithoutWhitespace( 'Payment request successfully reset')
-    resetPaymentRequestPage.successMessage().should('be.visible').containsWithoutWhitespace( 'Invoice number'); break
-  case 'what happens next subheader':
-    resetPaymentRequestPage.whatHappensNextSubheader().should('be.visible').containsWithoutWhitespace( 'What happens next'); break
-  case 'what happens next message':
-    resetPaymentRequestPage.whatHappensNextMessage().should('be.visible').containsWithoutWhitespace( 'The payment request will be reprocessed and resent to D365.'); break
-  case 'perform another action link':
-    resetPaymentRequestPage.performAnotherActionLink().should('be.visible').containsWithoutWhitespace( 'Perform another action'); break
+    resetPaymentRequestPage.successTitle().should('be.visible').containsWithoutWhitespace( 'has been successfully reset and will be reprocessed by Payment Hub'); break
   default:
     throw new Error('invalid element')
   }
@@ -1407,7 +1400,6 @@ Then (/^on the Reset payment request page I click the "(.*)"$/, (button) => {
 
   switch (button) {
   case 'reset button': resetPaymentRequestPage.resetButton().scrollIntoView().click(); break
-  case 'perform another action link': resetPaymentRequestPage.performAnotherActionLink().scrollIntoView().click(); break
   default:
     throw new Error('invalid button name')
   }
@@ -1415,31 +1407,56 @@ Then (/^on the Reset payment request page I click the "(.*)"$/, (button) => {
   console.log(`Clicked on the ${button} successfully`)
 })
 
-Then(/^I confirm that second completedPaymentRequest entry has been made in database for invoice number "(.*)"$/, (invoiceNumber) => {
+Then(
+  /^I confirm that second completedPaymentRequest entry has been made in database for invoice number "(.*)"$/,
+  (invoiceNumber) => {
+    Cypress.emit(
+      'log:step',
+      `I confirm that second completedPaymentRequest entry has been made in database for invoice number ${invoiceNumber}`
+    )
 
-  Cypress.emit('log:step', 'I confirm that second completedPaymentRequest entry has been made in database for invoice number ' + invoiceNumber)
+    if (invoiceNumber.includes('Current')) {
+      invoiceNumber = `S2795919${Cypress.env('nextContractNumber')}V001`
+    }
 
-  if (invoiceNumber.includes('Current')) {
-    invoiceNumber = 'S2795919' + Cypress.env('nextContractNumber').toString() + 'V001'
+    const sqlStatement = `
+      SELECT *
+      FROM "completedPaymentRequests"
+      WHERE "invoiceNumber" = '${invoiceNumber}'
+    `
 
+    const databaseName = 'ffc-pay-processing'
+
+    const pollForSecondEntry = (attempt = 1, maxAttempts = 20) => {
+      cy.task('databaseQuery', { env, databaseName, sqlStatement })
+        .then((result) => {
+          const data = result.rows
+
+          cy.log(
+            `Attempt ${attempt}: Found ${data.length} completedPaymentRequest entries for invoice number ${invoiceNumber}`
+          )
+
+          if (data.length >= 2) {
+            cy.log(
+              `Confirmed that second completedPaymentRequest entry has been made in database for invoice number ${invoiceNumber}`
+            )
+            return
+          }
+
+          if (attempt >= maxAttempts) {
+            throw new Error(
+              `Expected at least 2 completedPaymentRequest entries for invoice number ${invoiceNumber}, but found ${data.length}`
+            )
+          }
+
+          cy.wait(5000)
+          pollForSecondEntry(attempt + 1, maxAttempts)
+        })
+    }
+
+    pollForSecondEntry()
   }
-  const sqlStatement = `SELECT * FROM "completedPaymentRequests" WHERE "invoiceNumber" = '${invoiceNumber}'`
-  const databaseName = 'ffc-pay-processing'
-
-  cy.task('databaseQuery', { env, databaseName, sqlStatement })
-    .then((result) => {
-      const data = result.rows
-      cy.log(`Found ${data.length} completedPaymentRequest entries for invoice number ${invoiceNumber}`)
-      if (data.length >= 2) {
-        console.log(`Confirmed that second completedPaymentRequest entry has been made in database for invoice number ${invoiceNumber}`)
-        cy.log(`Confirmed that second completedPaymentRequest entry has been made in database for invoice number ${invoiceNumber}`)
-      } else {
-        console.log(`Expected at least 2 completedPaymentRequest entries for invoice number ${invoiceNumber}, but found ${data.length}`)
-        cy.log(`Expected at least 2 completedPaymentRequest entries for invoice number ${invoiceNumber}, but found ${data.length}`)
-        throw new Error('Second completedPaymentRequest entry has not been made in database')
-      }
-    })
-})
+)
 
 Then (/^on the View events page I confirm that "(.*)" is displayed$/, (element) => {
 
