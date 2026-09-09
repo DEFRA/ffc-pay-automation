122f60fafb10f5d3e66f1bdae8ced9d7a219a78b
SvendJK <47521171+SvendJK@users.noreply.github.com>
2026-09-09 10:55:57 +0100

    added getfuturedate and getDatePrior to date util helper

--- cypress/utils/date.js
+++ cypress/utils/date.js
@@ -13,3 +13,18 @@ export const resolveDate = (value) =>
   value === 'today'
     ? getTodayFormatted()
     : value
+
+export const getFutureDateParts = (days = 1) => ({
+  day: dayjs().add(days, 'day').format('DD'),
+  month: dayjs().add(days, 'day').format('MM'),
+  year: dayjs().add(days, 'day').format('YYYY')
+})
+
+export const getFormattedFutureDate = (days = 1) =>
+  dayjs().add(days, 'day').format('DD/MM/YYYY')
+
+export const getDatePriorTo = (date) => ({
+  day: dayjs(date, 'DD/MM/YYYY').subtract(1, 'day').format('DD'),
+  month: dayjs(date, 'DD/MM/YYYY').subtract(1, 'day').format('MM'),
+  year: dayjs(date, 'DD/MM/YYYY').subtract(1, 'day').format('YYYY')
+})
