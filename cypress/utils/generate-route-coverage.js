6159508edbd9d5728f97797d54df581353fe7967
SvendJK <47521171+SvendJK@users.noreply.github.com>
2026-09-15 15:08:25 +0100

    more additions to ignore list

--- cypress/utils/generate-route-coverage.js
+++ cypress/utils/generate-route-coverage.js
@@ -18,7 +18,8 @@ const repositories = [{
   source: 'routes'
 }]
 
-const ignoredRoutes = ['/', '/healthy', '/healthz', '/sitemap', '/robots.txt', '/assets/{path*}', '/static/{path*}', '/login', '/logout', '/dev-auth', '/authenticate']
+// ignore list of routes we don't really care about, not critical to business flows
+const ignoredRoutes = ['/', '/healthy', '/healthz', '/sitemap', '/robots.txt', '/assets/{path*}', '/static/{path*}', '/login', '/logout', '/dev-auth', '/authenticate', '/loading/{jobId}', '/download-report-list/generation/download/{jobId}', '/download-statements/download/{filename*}']
 
 const routeCoverageFile = path.join(
   process.cwd(),
